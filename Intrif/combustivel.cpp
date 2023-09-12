fd5c52e721c48b7764322fac6915b45d70d767d9
duows <henriquesouzaoficial004@gmail.com>
2023-09-11 23:53:01 -0300

    New Files

--- Intrif/combustivel.cpp
+++ Intrif/combustivel.cpp
@@ -6,34 +6,47 @@ using namespace std;
 int main()
 {
 
-    float v; cin >> v;
+    float v;
+    cin >> v;
 
-    if (v < 0 || v > pow(10,38)) {
+    if (v < 0 || v > pow(10, 38))
+    {
         return 0;
     }
 
-    float t; cin >> t;
+    float t;
+    cin >> t;
 
-    if (t < 0 || t > pow(10,38)) {
+    if (t < 0 || t > pow(10, 38))
+    {
         return 0;
     }
 
     float q = 0;
 
-    if (v == 0) {
+    if (v == 0)
+    {
         cout << "GASOLINA" << endl;
-    } else {
-        if (t == 0) {
-            cout << "ETANOL" << endl;
-        } else {
-            q = t/v;
-        }
     }
+    else
+    {
+        if (t == 0)
+        {
+            cout << "ETANOL" << endl;
+        }
+        else
+        {
+            q = t / v;
 
-    if (q < 0.7) {
-        cout << "ETANOL" << endl;
-    } else {
-        cout << "GASOLINA" << endl;
+            if (q < 0.7)
+            {
+                cout << "ETANOL" << endl;
+            }
+            else
+            {
+                cout << "GASOLINA" << endl;
+            }
+        }
     }
 
     return 0;
