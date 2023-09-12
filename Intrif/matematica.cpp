8aab4f3dada65dcf22a8b9b650074bae130c6b82
duows <henriquesouzaoficial004@gmail.com>
2023-09-11 23:48:13 -0300

    Matematica

--- Intrif/matematica.cpp
+++ Intrif/matematica.cpp
@@ -2,38 +2,8 @@
 
 using namespace std;
 
-int funcX(int x) {
-    return x-3;
-}
-
-int funcRecDificil(int x){
-    if (x >= 10000000) {
-        return funcX(x);
-    } else {
-        while (x < 10000000) {
-            x += 13;
-        }
-
-        return funcX(x);
-    }
-}
-
-
-
 int main()
 {
 
-    int x; cin >> x;
-
-    if (x <= 0 || x >= 20000000) {
-        return 0;
-    }
-
-    if (x >= 10000000) {
-        cout << funcX(x) << endl;
-    } else {
-        cout << funcRecDificil(x) << endl;
-    }
-    
     return 0;
 }
