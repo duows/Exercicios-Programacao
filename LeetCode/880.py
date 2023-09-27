b3b576187a4f7c2b2d812ae941922c31b954b032
duows <henriquesouzaoficial004@gmail.com>
2023-09-27 14:17:11 -0300

    New Files

---
+++ LeetCode/880.py
@@ -0,0 +1,14 @@
+s = "leet2code3"
+k = 10
+
+cont1 = 0
+s2 = ""
+
+while cont1 < len(s):
+    if s[cont1].isnumeric():
+        s2 = s2 * int(s[cont1])
+    else:
+        s2 += s[cont1]
+    cont1 += 1
+
+print(s2[k-1])
