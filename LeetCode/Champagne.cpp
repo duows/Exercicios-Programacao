85eaa07167d05d23b57b4c3dec8faad574a24e17
Jadir Pires de Borba Junior <62619866+JadirJunior@users.noreply.github.com>
2023-09-28 20:12:40 -0300

    Erro desconhecidooo

--- LeetCode/Champagne.cpp
+++ LeetCode/Champagne.cpp
@@ -3,45 +3,41 @@
 
 using namespace std;
 
-
-
 int main() {
 	
 	
-	vector< vector<double> > tacas;
-	
-	vector<double> vec1 = {1};
-	tacas.push_back(vec1);
-	
 	int poured = 2;
 	
-	
-	int limit = 2;
-	
-	for (int i = 0; i < 4;i++) {
-		//Inserindo 4 linhas
-		
-		vector<double> vec2;
-		
-		for (int j = 0; j < limit; j++) {
-			vec2.push_back(0);
-		}
-		
-		tacas.push_back(vec2);
-		
-		limit++;
-	}
-	
-	
-	int linha = 1;
-	
-	for (vector<double> vec : tacas) {
-		for (double val : vec) {
-			cout << val << " ";
-		}
-		cout << endl;
-	}
-	
+    vector<double> linhaAtual(1, poured);
+
+    int query_row = 1;
+    int query_glass = 1;
+
+    cout << "Poured: " << poured << endl;
+    
+    for (int i = 0; i < query_row; i++) {
+        
+        //Percorrendo as linhas
+        vector<double> prox(linhaAtual.size() + 1);
+
+        for (int j = 0; j < linhaAtual.size(); j++) {
+
+            //Percorrendo a linha atual
+            if (linhaAtual[j] >= 1) {
+
+                prox[j] = (linhaAtual[j]-1.0)/2.0;
+                prox[j+1] = (linhaAtual[j]-1.0)/2.0;
+
+                linhaAtual[j] = 1;
+            } 
+
+        }
+
+        linhaAtual = prox;
+    }
+
+
+    cout << linhaAtual[query_glass] << endl;
 	
 	
 	
