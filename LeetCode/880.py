5c61011aecc1d25b8830abc712746d160b33abe3
duows <henriquesouzaoficial004@gmail.com>
2023-09-27 21:48:36 -0300

    new files

--- LeetCode/880.py
+++ LeetCode/880.py
@@ -1,16 +1,17 @@
-class Solution:
-    def decodeAtIndex(self, s: str, k: int) -> str:
-        cont1 = 0
-        s2 = ""
+s = "leet2code3"
+k = 10
 
-        if k == 1:
-            return s[0]
+cont1 = 0
+s2 = ""
+
+if k == 1:
+    print(s[0]) 
+else:
+    while cont1 < len(s):
+        if s[cont1].isnumeric():
+            s2 = s2 * int(s[cont1])
         else:
-            while cont1 < len(s):
-                if s[cont1].isnumeric():
-                    s2 = s2 * int(s[cont1])
-                else:
-                    s2 += s[cont1]
-                cont1 += 1
-            
-            return s2[k-1]
+            s2 += s[cont1]
+        cont1 += 1
+    
+print(s2[k-1])
