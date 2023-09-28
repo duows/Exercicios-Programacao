d7b68955ff1ea342d889078c7c4d87cfb723bc84
Jadir Pires de Borba Junior <62619866+JadirJunior@users.noreply.github.com>
2023-09-27 23:55:51 -0300

    Código corrigido

--- LeetCode/09_27.cpp
+++ LeetCode/09_27.cpp
@@ -1,86 +1,40 @@
 #include <iostream>
 #include <string>
 
-
-#define INT long long int
-
 using namespace std;
 
-
-void escreverString(string *aux, string *repeticoes, string *decoded) {
-    cout << *repeticoes << endl;
+string decodeAtIndex(string s, int k) {
+    long long size = 0;
+    int n = s.size();
+    for (int i = 0; i < n; i++) {
+        if (isdigit(s[i])) { 
+            size *= s[i] - '0';
+            
+        } else {
+            size++;
+        }
+    }
     
-	int repeticoes2 = int stoi(*repeticoes);
-	cout << "A2" << endl;
-	while (repeticoes2 > 0) {
-		repeticoes2--;
-		*decoded = (*decoded) + (*aux);
-	}
-	
-	*aux = "";
-	*repeticoes = "";
+    for (int i = n - 1; i >= 0; i--) {
+        k =k% size;
+
+        if (k == 0 && isalpha(s[i])) {
+            return string(1, s[i]);
+        }
+        if (isdigit(s[i])) {
+            size /= s[i] - '0';
+        } else {
+            size--;
+        }
+    }
+
+    return "";
 }
 
 int main()
 {
-	
-	string s = "a2345678999999999999999";
-	cout << "A1" << endl;
-	
-	int k = 1;
-	
-	string decoded = "", aux = "", repeticoes = "", res = "";
-	
-	bool comecouContagem = false;
-	int cont = 0;
-	for (char c : s) {
-			
-		if ( ((int) c >= 97 && (int) c <= 122) ) {
-			//Caso seja um dígito
-			
-			if (comecouContagem) {
-				comecouContagem = false;
-				
-				escreverString(&aux, &repeticoes, &decoded);
-				
-			}
-			
-			aux += c;
-			
-		} else {
-			
-			
-			//Caso seja um número
-			
-			if (!comecouContagem) {
-				
-				comecouContagem = true;
-				repeticoes = c;
-				
-			} else {
-				repeticoes += c;
-			}
-			
-			if (cont == s.length() - 2) {
-				//Caso o dígito seja a penúltima letra
-				escreverString(&aux, &repeticoes, &decoded);
-				
-			} else if (cont == s.length()-1) {
-				string aux2 = decoded+aux;
-				escreverString(&aux2, &repeticoes, &res);
-			}
-		}
-		
-		cont++;
-			
-	}
-	
-	char caractere = res[k-1];
-	
-	cout << caractere << endl;
-	
-	cout << res << endl;
-    
+    string s = "leet2code3";
+    int k = 10;
+    cout << decodeAtIndex(s, k) << endl;
     return 0;
 }
-
