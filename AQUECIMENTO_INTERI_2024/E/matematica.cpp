dfc0604cb2cc85f406dd9d56d408affeed3f951f
JadirJunior <jadirjunior8@gmail.com>
2024-07-24 00:06:40 -0300

    Letra M Runtime Error

--- AQUECIMENTO_INTERI_2024/E/matematica.cpp
+++ AQUECIMENTO_INTERI_2024/E/matematica.cpp
@@ -3,9 +3,6 @@
 
 using namespace std;
 
-
-//RUNTIME ERROR ???
-
 long long int f(long long int x) {
     while (x < 10000000) {
         x += 13 * 3;
---
+++ AQUECIMENTO_INTERI_2024/M/tudo.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N, M; 
+    cin >> N >> M;
+    int L; 
+    cin >> L;
+
+    vector<vector<int>> fluxos(N, vector<int>(L, 0));
+
+    // Lê os valores e separa nos respectivos vetores de fluxo
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < L; j++) {
+            int valor; 
+            cin >> valor;
+            fluxos[i][j] = valor;
+        }
+    }
+
+    int quadroAtual = 1;
+    
+    for (int posInicial = 0; posInicial < L; posInicial += M) {
+        long long int somaQuadro = 0;
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                int pos = posInicial + j;
+                if (pos < L) {
+                    cout << fluxos[i][pos] << endl;
+                    somaQuadro += fluxos[i][pos];
+                }
+            }
+        }
+
+        long long int mod = somaQuadro % 256;
+        cout << mod << endl;
+
+        quadroAtual++;
+    }
+
+    return 0;
+}
