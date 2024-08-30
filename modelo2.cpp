4c19a080898109dedad1e4e0cd80cbc0a86d81d3
Henrique Souza <102422550+duows@users.noreply.github.com>
2024-08-30 20:17:59 -0300

    Create modelo2.cpp

---
+++ modelo2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* macro para código mais rapido e para \n
+#define _ ios_base::sync_with_stdio(0);cin.tie(0); //n usar printf junto com cout
+//^comentar _ para degubar código
+#define endl '\n'
+*/
+
+/* macro para trabalhar com pair
+#define f first
+#define s second
+*/
+
+/* funcao para debugging
+#define dgb(x) cout << #x << " = " << x << endl;
+*/
+
+int main() { _
+
+	return 0;
+}
